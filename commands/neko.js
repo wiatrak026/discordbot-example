52452d44809c99d075f61559f310005f7c335e15
Taki Tam Slu <null>
2020-07-24 19:45:42 +0200

    Komenda Neko

--- commands/neko.js
+++ commands/neko.js
@@ -1,7 +1,12 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (_client, message) => {
-
+    fetch("https://nekobot.xyz/api/image?type=neko").then((res) => res.json()).then((body) => {
+        const embed = new Discord.MessageEmbed()
+        .setColor(body.color)
+        .setImage(body.message);
+        message.channel.send(embed);
+    }).catch(() => message.channel.send("❎ **Błąd API!**"));
 };
 
 module.exports.help = {
